4fef564ea01b6ee7c0ebc434fa3b284aecc6eaf0
Rens Mulder <mulderrens@outlook.com>
2017-03-04 15:00:11 +0100

    Added 1 word

--- GHE/src/nl/gewoonhdgaming/essentials/Main.java
+++ GHE/src/nl/gewoonhdgaming/essentials/Main.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	
 	public void onEnable() {
-		getLogger().info("Booted up!");
+		getLogger().info("Booted up succesfull!");
 	}
 
 }
